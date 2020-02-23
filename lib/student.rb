d7458b4591634f7d4dcdf76e922203df0d532e45
fadimata tillman <fadi.tillman@gmail.com>
2020-02-23 01:34:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@
 class Student
 
   def hello
-    @hello = returns "Hey there! I'm so excited to learn stuff."
+    @hello = return "Hey there! I'm so excited to learn stuff."
 end
 
 def raise_hand
