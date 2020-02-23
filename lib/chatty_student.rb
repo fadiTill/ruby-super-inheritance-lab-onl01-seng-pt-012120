84cb4ad1e0e73e982c5a7e9261fef0a8e4331903
fadimata tillman <fadi.tillman@gmail.com>
2020-02-23 01:41:55 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,10 @@
  class ChattyStudent < Student
 
-   def chatty_student
+   def hello
      super
-     @hello =   "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+     puts  "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
  end
  end
+
+def raise_hand
+  10.times do
